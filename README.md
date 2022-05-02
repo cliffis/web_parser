9000a65758f72a4c9060ffabe794fbad12a0c5f0
cliffis <cliffis@yandex.ru>
2022-05-02 19:22:23 +0200

    feat: create README.md, requirements.txt

---
+++ README.md
@@ -0,0 +1,11 @@
+# Website parser.
+---
+Script for parsing the menu from the restaurant website.
+The script searches for all offered dishes containing a component named "csirkemell" (chicken breast),
+and then outputs the result of the cheapest dish for each day of the week to a file.
+---
+####  Sample output:
++ Day: Péntek
++ Date: 05.13
++ Name: Majonézes burgonya, natúr csirkemell csíkokkal
++ Price: 620 Ft
--- parser_menu.py
+++ parser_menu.py
@@ -1,14 +1,14 @@
-#Developer: Seriakov Igor
+# Developer: Seriakov Igor
 #######################################################################################################
-#Script for parsing the menu from the restaurant website(https://www.teletal.hu/etlap/19).
-#The script searches for all offered dishes containing a component named "csirkemell" (chicken breast),
-#and then outputs the result of the cheapest dish for each day of the week to a file.
+# Script for parsing the menu from the restaurant website(https://www.teletal.hu/etlap/19).
+# The script searches for all offered dishes containing a component named "csirkemell" (chicken breast),
+# and then outputs the result of the cheapest dish for each day of the week to a file.
 #######################################################################################################
-#Sample output:
-#Day: Péntek
-#Date: 05.13
-#Name: Majonézes burgonya, natúr csirkemell csíkokkal
-#Price: 620 Ft
+# Sample output:
+#  Day: Péntek
+#  Date: 05.13
+#  Name: Majonézes burgonya, natúr csirkemell csíkokkal
+#  Price: 620 Ft
 #######################################################################################################
 
 
